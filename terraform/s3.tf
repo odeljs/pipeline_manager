52e72ff1f96ec89b13cc1e26174ef9f78d7627a4
Justin Odel <odel.justin@gmail.com>
2026-09-04 14:51:38 -0400

    including tf plan output bucket

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -14,9 +14,25 @@ resource "aws_s3_bucket_public_access_block" "teraform_plan_artifacts_bucket_pab
 
 ########################################################################
 
-###################tf-plan artifact bucket #########################
+###################tf-plan input bucket #########################
 resource "aws_s3_bucket" "teraform_plan_input_bucket" {
   bucket = "tfplan-input-bucket"
+}
+
+resource "aws_s3_bucket_public_access_block" "teraform_plan_input_bucket_pab" {
+  bucket = aws_s3_bucket.teraform_plan_input_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+########################################################################
+
+###################tf-plan input bucket #########################
+resource "aws_s3_bucket" "teraform_plan_output_bucket" {
+  bucket = "tfplan-output-bucket"
 }
 
 resource "aws_s3_bucket_public_access_block" "teraform_plan_input_bucket_pab" {
